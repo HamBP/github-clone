67485287af07f37f90066c5ac8f3c073c5b5d4b4
algosketch <sjy_9484@naver.com>
2021-05-11 02:18:14 +0900

    fix header css

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,21 +1,36 @@
-import { MdArrowDropDown } from "react-icons/md"
+import { MdArrowDropDown } from "react-icons/md";
+import { BsBell } from "react-icons/bs";
+import { CgMathPlus } from "react-icons/cg";
+import { AiFillGithub } from "react-icons/ai";
 
 function Header() {
   return (
     <header>
       <div className='header'>
-        <img src=''/>
-        <input className='search-bar' placeholder='Search or jum to...'/>
+        <div className="header-left">
+          <a href="https://github.com/" id="github-home"><AiFillGithub class="github-icon"/></a>
+          <form action="" className='search-bar'>
+            <input id="search-bar" placeholder='Search or jum to...'/>
+          </form>
 
-        <a href=''>Pull requests</a>
-        <a href=''>Issues</a>
-        <a href=''>Marketplace</a>
-        <a href=''>Explore</a>
+          <div className="nav">
+            <a href=''>Pull requests</a>
+            <a href=''>Issues</a>
+            <a href=''>Marketplace</a>
+            <a href=''>Explore</a>
+          </div>
 
-        <img src='./bell.png'/>
-        <img src=''/>
-        <img src="" alt="" className="profile"/>
-        <MdArrowDropDown onClick={dropDown} className="btn-drop-down"/>
+        </div>
+
+
+        <div className="header-right">
+          <BsBell className="bell"/>
+          <CgMathPlus className="plus"/>
+          <MdArrowDropDown className="btn-drop-down"/>
+          <img src="https://avatars.githubusercontent.com/u/35232655?s=60&v=4" alt="" className="profile"/>
+          <MdArrowDropDown onClick={dropDown} className="btn-drop-down"/>
+        </div>
+
       </div>
     </header>
   );
--- src/components/index/Main.js
+++ src/components/index/Main.js
@@ -34,11 +34,11 @@ function Main() {
           <img src="" alt=""/>
         </div>
         <div>
-          <div><a href="">bbaktaeho</a> created a repository <a href="">my-project-storage/web-linkedin</a> 9 hours ago</div>
+          <div><a href="">potados</a> started following <a href="">velopert</a> 3 days ago</div>
           <div className="card feed">
-            <a href="">my-project-storage/web-linkedin</a>
-            <p>난이도: ★★★★</p>
-            <span>Updated May 10</span>
+            <a href=""><strong>Minjun Kim</strong></a> <a>velopert</a>
+            <p>개발은 언제나 즐겁고 재밌어야 한다는 생각을 갖고 있는 개발자이며, 가르치는것을 굉장히 좋아하는 교육자이기도 합니다.</p>
+            <a href="">76 repositories</a> <a href="">2.1k followers</a>
           </div>
         </div>
       </div>
@@ -48,11 +48,11 @@ function Main() {
           <img src="" alt=""/>
         </div>
         <div>
-          <div><a href="">bbaktaeho</a> created a repository <a href="">my-project-storage/web-linkedin</a> 9 hours ago</div>
+          <div><a href="">orihehe</a> starred <a href="">738/awesome-devbook</a> 3 days ago</div>
           <div className="card feed">
-            <a href="">my-project-storage/web-linkedin</a>
-            <p>난이도: ★★★★</p>
-            <span>Updated May 10</span>
+            <a href="">738/awesome-devbook</a>
+            <p><img src="https://github.githubassets.com/images/icons/emoji/unicode/1f4d5.png" alt=""/>국내 개발 도서 리스트</p>
+            <a href="">☆ 398</a> <span>Updated May 10</span>
           </div>
         </div>
       </div>
