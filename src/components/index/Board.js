9a6e1a1ab707d3070417b39fc53044e7abae9504
algosketch <sjy_9484@naver.com>
2021-05-11 02:45:56 +0900

    fix Board(left aside) css

--- src/components/index/Board.js
+++ src/components/index/Board.js
@@ -1,33 +1,45 @@
+import { MdArrowDropDown } from "react-icons/md";
+import { AiOutlineBook } from "react-icons/ai";
+
 function Board() {
   return (
     <div className='board'>
       <div className='dashboard-context'>
-        <img src='' />
-        <span>HamBP</span>
-        <img src='' />
+        <img className="mini-profile" src="https://avatars.githubusercontent.com/u/35232655?s=60&v=4" alt=""/>
+        <span className="nickname">HamBP</span>
+        <MdArrowDropDown className="dropdown"/>
       </div>
-      <hr />
 
-      <div>
+      <div class="repository-title">
         <h2>Repositories</h2>
         <a href=''>New</a>
       </div>
 
       <input id="find-repository" placeholder='Find a repository...' />
 
-      <a href=''>inu-appcenter/INU-emotion-android</a>
-      <a href=''>HamBP/chat-unavailable</a>
-      <a href=''>INU-Fake-Develo... /IamJunYoung</a>
+      <div className="repositories">
+        <div className={"flex"}>
+          <AiOutlineBook className={"book"}/><a href=''>inu-appcenter/INU-emotion-android</a>
+        </div>
+        <div className={"flex"}>
+          <AiOutlineBook className={"book"}/><a href=''>HamBP/chat-unavailable</a>
+        </div>
+        <div className={"flex"}>
+          <AiOutlineBook className={"book"}/><a href=''>INU-Fake-Develo... /IamJunYoung</a>
+        </div>
+      </div>
 
       <button id="btn-show-more" type='submit'>show more</button>
       <hr />
 
       <h2>Your teams</h2>
       <input placeholder='Find a team...' />
-      <ul>
-        <li>INU-Fake-Developers/season-2</li>
-        <li>inu-appcenter/android</li>
-      </ul>
+      <div className="flex">
+        <a href="">INU-Fake-Developers/season-2</a>
+      </div>
+      <div className="flex">
+        <a href="">inu-appcenter/android</a>
+      </div>
     </div>
   );
 }
