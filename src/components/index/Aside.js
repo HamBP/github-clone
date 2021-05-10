ab7be375334fc3c9a790e206f425df5f001d80d7
algosketch <sjy_9484@naver.com>
2021-05-11 03:27:17 +0900

    fix aside css

--- src/components/index/Aside.js
+++ src/components/index/Aside.js
@@ -1,7 +1,10 @@
+import {BsMoon} from "react-icons/bs";
+
 function Aside() {
   return (
     <aside>
       <div className="dark-mode card">
+        <BsMoon className={"moon"}/>
         <h3>Dark mode is here!</h3>
         <p>
           Go to Settings → Appearance to choose your theme preference.
@@ -10,18 +13,24 @@ function Aside() {
       </div>
 
       <h2>Explore repositories</h2>
-      <div>
-        <a href="">portfolio-toy/soptConnector</a>
+
+      <div className="repositories">
+        <div className={"repository divider"}>
+          <a href="">codestates/lilakchal-client</a>
+          <img className="typescript divider"></img><span>Typescript</span> <span>☆2</span>
+        </div>
+        <div className={"repository divider"}>
+          <a href="">vidit135g/Floral</a>
+          <img className="javascript divider"></img><span>Javascript</span> <span>☆4</span>
+        </div>
+        <div className={"repository"}>
+          <a href="">swmTeam-29/main-container</a>
+          <p>멘토링 신청/취소와 멘토링 한줄평 기능을 제공하는 챗봇 애기 멘티 소마!</p>
+          <img className="javascript"></img><span>Javascript</span> <span>☆4</span>
+        </div>
       </div>
-      <div>
-        <a href="">vidit135g/Floral</a>
-        <p>Minimal design gallery app for Android</p>
-      </div>
-      <div>
-        <a href="">muzei/muzei</a>
-        <p>Muzei Live Wallpaper for Android</p>
-      </div>
-      <a href="">Explore more →</a>
+
+      <a id="explore-more" href="">Explore more →</a>
     </aside>
   );
 }
